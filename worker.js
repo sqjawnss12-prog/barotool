74d3f5b635914bf05469d02f17fa20c71080d63b
sqjawnss12-prog <sqjawnss12@gmail.com>
2026-09-09 07:03:59 +0900

    Track FloorFit daily views

--- worker.js
+++ worker.js
@@ -11,7 +11,7 @@ export class ViewCounter {
 }
 const HOME_SITES = new Map([
   ['/', 'barotool'], ['/marketing', 'marketing'], ['/marketing/', 'marketing'],
-  ['/areafit', 'areafit'], ['/areafit/', 'areafit'],
+  ['/areafit', 'areafit'], ['/areafit/', 'areafit'], ['/floorfit', 'floorfit'], ['/floorfit/', 'floorfit'],
   ['/packfit', 'packfit'], ['/packfit/', 'packfit'], ['/tilefit', 'tilefit'], ['/tilefit/', 'tilefit'],
   ['/powercost', 'powercost'], ['/powercost/', 'powercost'], ['/curtainfit', 'curtainfit'], ['/curtainfit/', 'curtainfit'],
   ['/paintfit', 'paintfit'], ['/paintfit/', 'paintfit'], ['/wallfit', 'wallfit'], ['/wallfit/', 'wallfit']
