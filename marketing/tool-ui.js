5e77f5429c6e164982f817ccc05062b36aebfa17
sqjawnss12-prog <sqjawnss12@gmail.com>
2026-09-11 17:03:37 +0900

    Add max discount calculator to marketing navigation

--- marketing/tool-ui.js
+++ marketing/tool-ui.js
@@ -30,6 +30,7 @@
     ]],
     ['마진·가격',[
       ['/marketing/product-profit.html','상품 마진'],
+      ['/marketing/max-discount-rate.html','최대 할인율'],
       ['/marketing/bulk-profit.html','상품 마진 대량 분석'],
       ['/marketing/target-price.html','목표 판매가 역산'],
       ['/marketing/profit-buffer.html','마진 안전구간']
