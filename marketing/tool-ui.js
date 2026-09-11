4ac61b6442482800188e5eae1c62b28376f4e2be
sqjawnss12-prog <sqjawnss12@gmail.com>
2026-09-11 18:59:34 +0900

    Link coupon cost share calculator in marketing navigation

--- marketing/tool-ui.js
+++ marketing/tool-ui.js
@@ -32,6 +32,7 @@
       ['/marketing/product-profit.html','상품 마진'],
       ['/marketing/max-discount-rate.html','최대 할인율'],
       ['/marketing/discount-volume-breakeven.html','할인 판매량 손익분기'],
+      ['/marketing/coupon-cost-share.html','쿠폰 비용 분담'],
       ['/marketing/bulk-profit.html','상품 마진 대량 분석'],
       ['/marketing/target-price.html','목표 판매가 역산'],
       ['/marketing/profit-buffer.html','마진 안전구간']
